fc732f5cd00da4a449003c3c4e71941f18401d0a
nacal <nacal.dev@gmail.com>
2023-04-22 02:42:33 +0900

    add storybook settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import '../src/styles/tailwind.css'
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +9,19 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#D7D9D8',
+        },
+        {
+          name: 'dark',
+          value: '#303633',
+        },
+      ],
     },
   },
 }
