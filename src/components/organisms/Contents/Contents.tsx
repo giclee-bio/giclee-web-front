e5cbc72d1000f7c243acf432a482816f04233da5
nacal <nacal.dev@gmail.com>
2023-04-24 01:04:02 +0900

    add wide option to  Contents component

--- src/components/organisms/Contents/Contents.tsx
+++ src/components/organisms/Contents/Contents.tsx
@@ -1,9 +1,14 @@
+import classNames from 'classnames'
+
 type Props = {
   children: React.ReactNode
+  wide?: boolean
 }
 
-const Contents: React.FC<Props> = ({ children }) => {
-  return <div className='mx-auto max-w-4xl pt-8'>{children}</div>
+const Contents: React.FC<Props> = ({ children, wide }) => {
+  const classes = classNames(wide ? 'max-w-7xl' : 'max-w-4xl')
+
+  return <div className={`mx-auto px-4 pt-8 ${classes}`}>{children}</div>
 }
 
 export default Contents
