f70f746b8c58eb5d0ce3ba050a76f76a68f2f96a
nacal <nacal.dev@gmail.com>
2023-04-30 16:43:12 +0900

    fix

--- src/components/example/Example.test.tsx
+++ src/components/example/Example.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 
 import Example from './Example'
 
-describe('Button', () => {
+describe('Example', () => {
   it('snapshot', async () => {
     const { asFragment } = render(<Example />, {})
     expect(asFragment()).toMatchSnapshot()
