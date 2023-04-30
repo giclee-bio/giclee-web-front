ae83a7e907642af32ec52cab890a0c798c407ab9
nacal <nacal.dev@gmail.com>
2023-05-01 00:41:38 +0900

    fix Contents component

--- src/components/organisms/Contents/Contents.tsx
+++ src/components/organisms/Contents/Contents.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Contents: React.FC<Props> = ({ center, children, wide }) => {
   const classes = classNames(
-    center ? 'flex min-h-screen items-center justify-center' : 'pt-8',
+    center ? 'flex min-h-screen items-center justify-center flex-col' : 'pt-8',
     wide ? 'max-w-7xl' : 'max-w-4xl'
   )
 
