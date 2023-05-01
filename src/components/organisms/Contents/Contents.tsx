20986ec96dde3138392593f140a502d332574e5c
nacal <nacal.dev@gmail.com>
2023-05-02 01:10:59 +0900

    Develop Content component. Add width option props.

--- src/components/organisms/Contents/Contents.tsx
+++ src/components/organisms/Contents/Contents.tsx
@@ -3,14 +3,24 @@ import classNames from 'classnames'
 type Props = {
   center?: boolean
   children: React.ReactNode
-  wide?: boolean
+  width?: 'wide' | 'narrow'
 }
 
-const Contents: React.FC<Props> = ({ center, children, wide }) => {
-  const classes = classNames(
-    center ? 'flex min-h-screen items-center justify-center flex-col' : 'pt-8',
-    wide ? 'max-w-7xl' : 'max-w-4xl'
+const Contents: React.FC<Props> = ({ center, children, width }) => {
+  let classes = classNames(
+    center ? 'flex min-h-screen items-center justify-center flex-col' : 'pt-8'
   )
+
+  switch (width) {
+    case 'wide':
+      classes = classNames(classes, 'max-w-7xl')
+      break
+    case 'narrow':
+      classes = classNames(classes, 'max-w-xl')
+      break
+    default:
+      classes = classNames(classes, 'max-w-4xl')
+  }
 
   return <div className={`mx-auto px-4 ${classes}`}>{children}</div>
 }
