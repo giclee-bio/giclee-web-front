f80ee13b9629e79fa473bb8f7c473720c6eff240
nacal <nacal.dev@gmail.com>
2023-05-02 01:11:43 +0900

    Create account page.

---
+++ src/app/(others)/account/page.tsx
@@ -0,0 +1,10 @@
+import Contents from '@/components/organisms/Contents'
+import Account from '@/components/pages/Account'
+
+export default function Home() {
+  return (
+    <Contents width='narrow'>
+      <Account />
+    </Contents>
+  )
+}
