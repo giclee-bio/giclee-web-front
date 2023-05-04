a9fa7147b67bf0218679e3ec489a18c86b4cd65d
nacal <nacal.dev@gmail.com>
2023-05-04 14:35:46 +0900

    Import empty components.

--- src/components/pages/Card/Card.tsx
+++ src/components/pages/Card/Card.tsx
@@ -1,5 +1,17 @@
+import CardContent from '@/components/organisms/CardContent'
+import EditableCardItemsList from '@/components/organisms/EditableCardItemsList'
+import SearchPlatformItemsModule from '@/components/organisms/SearchPlatformItemsModule'
+
 const Card = () => {
-  return <>Card</>
+  return (
+    <div>
+      <div>
+        <SearchPlatformItemsModule />
+        <EditableCardItemsList />
+      </div>
+      <CardContent />
+    </div>
+  )
 }
 
 export default Card
