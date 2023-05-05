c881dbd9bbecdd32eacdc51cd21be587f76ca302
nacal <nacal.dev@gmail.com>
2023-05-05 17:04:17 +0900

    DEvelop EditableCardItemsList component.

--- src/components/organisms/EditableCardItemsList/EditableCardItemsList.test.tsx
+++ src/components/organisms/EditableCardItemsList/EditableCardItemsList.test.tsx
@@ -2,9 +2,18 @@ import { render } from '@testing-library/react'
 
 import EditableCardItemsList from './EditableCardItemsList'
 
+import type { PlatformItems } from '@/components/atoms/PlatformItem'
+
+const item = {
+  alt: 'nacal',
+  caption: 'nacal',
+  src: '/nacal.png',
+}
+const items: PlatformItems[] = new Array(8).fill(item)
+
 describe('EditableCardItemsList', () => {
   it('snapshot', async () => {
-    const { asFragment } = render(<EditableCardItemsList />, {})
+    const { asFragment } = render(<EditableCardItemsList items={items} />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 })
--- src/components/organisms/EditableCardItemsList/EditableCardItemsList.tsx
+++ src/components/organisms/EditableCardItemsList/EditableCardItemsList.tsx
@@ -1,5 +1,31 @@
-const EditableCardItemsList = () => {
-  return <>EditableCardItemsList</>
+'use client'
+
+import * as RadixForm from '@radix-ui/react-form'
+
+import Button from '@/components/atoms/Button'
+import EditableCardItem from '@/components/atoms/EditableCardItem'
+
+import type { PlatformItems } from '@/components/atoms/PlatformItem'
+
+type Props = {
+  items: PlatformItems[]
+}
+
+const EditableCardItemsList: React.FC<Props> = ({ items }) => {
+  return (
+    <RadixForm.Root className='mt-4 max-w-md'>
+      <ol className='min-h-[320px]'>
+        {items.map((item, key) => {
+          return <EditableCardItem {...item} key={key} />
+        })}
+      </ol>
+      <RadixForm.Submit asChild>
+        <Button className='mx-auto mt-8 block' href='' type='primary'>
+          保存
+        </Button>
+      </RadixForm.Submit>
+    </RadixForm.Root>
+  )
 }
 
 export default EditableCardItemsList
