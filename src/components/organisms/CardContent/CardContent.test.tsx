f9e380f5355429c00f2b0de96db0182564b84b1e
nacal <nacal.dev@gmail.com>
2023-05-05 18:40:57 +0900

    Develop CardContent component.

--- src/components/organisms/CardContent/CardContent.test.tsx
+++ src/components/organisms/CardContent/CardContent.test.tsx
@@ -2,9 +2,37 @@ import { render } from '@testing-library/react'
 
 import CardContent from './CardContent'
 
+const account = {
+  description: 'よろしくお願いします！',
+  displayName: 'nacal',
+  iconImageSrc: '/nacal.png',
+  id: 'nacal',
+}
+
+const items = [
+  {
+    alt: 'nacal',
+    caption: 'nacal',
+    platformAccountName: 'aaaa',
+    src: '/nacal.png',
+  },
+  {
+    alt: 'nacal2',
+    caption: 'nacal2',
+    platformAccountName: 'bbbb',
+    src: '/nacal.png',
+  },
+  {
+    alt: 'nacal3',
+    caption: 'nacal3',
+    platformAccountName: 'cccc',
+    src: '/nacal.png',
+  },
+]
+
 describe('CardContent', () => {
   it('snapshot', async () => {
-    const { asFragment } = render(<CardContent />, {})
+    const { asFragment } = render(<CardContent account={account} items={items} />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 })
--- src/components/organisms/CardContent/CardContent.tsx
+++ src/components/organisms/CardContent/CardContent.tsx
@@ -1,5 +1,34 @@
-const CardContent = () => {
-  return <>CardContent</>
+import Image from 'next/image'
+
+import type { PlatformItems } from '@/components/atoms/PlatformItem'
+import type { Account } from '@/components/pages/Card'
+
+type Props = {
+  account: Account
+  items: PlatformItems[]
+}
+
+const CardContent: React.FC<Props> = ({ account, items }) => {
+  return (
+    <div className='h-[667px] w-[375px] rounded-lg bg-stone-800'>
+      <div className='mt-16 flex flex-col items-center'>
+        <Image
+          alt={account.displayName}
+          className='rounded-full'
+          height='72'
+          src={account.iconImageSrc}
+          width='72'
+        />
+        <h2 className='mt-2 text-lg font-bold'>{account.displayName}</h2>
+      </div>
+      {items.map((item, key) => (
+        <li className='mt-8 flex list-none items-center justify-center' key={key}>
+          <Image alt={item.alt} className='rounded' height='36' src={item.src} width='36' />
+          <p className='ml-4'>{item.platformAccountName}</p>
+        </li>
+      ))}
+    </div>
+  )
 }
 
 export default CardContent
