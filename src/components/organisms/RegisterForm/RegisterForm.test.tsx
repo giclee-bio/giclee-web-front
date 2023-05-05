34567d7930969951fa436d2d64d8c9b182df7a10
yoshinoseiji <sij.ysn25@gmail.com>
2023-05-05 19:38:30 +0900

    test: テストを修正

--- src/components/organisms/RegisterForm/RegisterForm.test.tsx
+++ src/components/organisms/RegisterForm/RegisterForm.test.tsx
@@ -1,10 +1,16 @@
 import { render } from '@testing-library/react'
+import { SessionProvider } from 'next-auth/react'
 
 import RegisterForm from './RegisterForm'
 
 describe('RegisterForm', () => {
   it('snapshot', async () => {
-    const { asFragment } = render(<RegisterForm />, {})
+    const { asFragment } = render(
+      <SessionProvider>
+        <RegisterForm />
+      </SessionProvider>,
+      {}
+    )
     expect(asFragment()).toMatchSnapshot()
   })
 })
