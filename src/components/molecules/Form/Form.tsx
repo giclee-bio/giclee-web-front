644cb22dc9774c64fe1525bdda4278fb4d985950
yoshinoseiji <sij.ysn25@gmail.com>
2023-05-05 18:43:39 +0900

    feat: inputのデータをキャッチ

--- src/components/molecules/Form/Form.tsx
+++ src/components/molecules/Form/Form.tsx
@@ -88,7 +88,14 @@ const FormItem: React.FC<FormField> = ({
 
 const Form: React.FC<Props> = ({ buttonCenter = true, buttonProps, formFields }) => {
   return (
-    <RadixForm.Root className='w-64'>
+    <RadixForm.Root
+      className='w-64'
+      onSubmit={(event) => {
+        event.preventDefault()
+        const data = Object.fromEntries(new FormData(event.currentTarget))
+        console.log(data)
+      }}
+    >
       {formFields.map((formFieid, key) => {
         return <FormItem {...formFieid} key={key} />
       })}
