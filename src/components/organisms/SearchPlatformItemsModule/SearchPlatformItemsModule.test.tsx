e1c93b6036db9e634650d87f248d9c98672af280
nacal <nacal.dev@gmail.com>
2023-05-05 18:56:48 +0900

    snapshot

--- src/components/organisms/SearchPlatformItemsModule/SearchPlatformItemsModule.test.tsx
+++ src/components/organisms/SearchPlatformItemsModule/SearchPlatformItemsModule.test.tsx
@@ -2,9 +2,33 @@ import { render } from '@testing-library/react'
 
 import SearchPlatformItemsModule from './SearchPlatformItemsModule'
 
+const items = [
+  {
+    alt: 'nacal',
+    caption: 'nacal',
+    platformAccountName: 'aaaa',
+    src: '/nacal.png',
+  },
+  {
+    alt: 'nacal2',
+    caption: 'nacal2',
+    platformAccountName: 'bbbb',
+    src: '/nacal.png',
+  },
+  {
+    alt: 'nacal3',
+    caption: 'nacal3',
+    platformAccountName: 'cccc',
+    src: '/nacal.png',
+  },
+]
+
 describe('SearchPlatformItemsModule', () => {
   it('snapshot', async () => {
-    const { asFragment } = render(<SearchPlatformItemsModule />, {})
+    const { asFragment } = render(
+      <SearchPlatformItemsModule handleSelectItem={() => jest.mock} items={items} />,
+      {}
+    )
     expect(asFragment()).toMatchSnapshot()
   })
 })
