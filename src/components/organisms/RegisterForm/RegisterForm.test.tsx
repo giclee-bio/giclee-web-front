c620e5a8b49b60b3676aba315ba22d76d0bff7cc
yoshinoseiji <sij.ysn25@gmail.com>
2023-05-05 20:27:03 +0900

    test: sessionをモック

--- src/components/organisms/RegisterForm/RegisterForm.test.tsx
+++ src/components/organisms/RegisterForm/RegisterForm.test.tsx
@@ -1,12 +1,19 @@
 import { render } from '@testing-library/react'
 import { SessionProvider } from 'next-auth/react'
+import type { Session } from 'next-auth'
 
 import RegisterForm from './RegisterForm'
 
 describe('RegisterForm', () => {
   it('snapshot', async () => {
+    const session: Session = {
+      user: {
+        name: '',
+      },
+      expires: new Date().toISOString(),
+    }
     const { asFragment } = render(
-      <SessionProvider>
+      <SessionProvider session={session}>
         <RegisterForm />
       </SessionProvider>,
       {}
