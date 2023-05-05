2aafc062d00381502c712d6b8af9548533871297
nacal <nacal.dev@gmail.com>
2023-05-05 18:40:38 +0900

    Develop PlatformItem component. Add link.

--- src/components/atoms/PlatformItem/PlatformItem.tsx
+++ src/components/atoms/PlatformItem/PlatformItem.tsx
@@ -3,15 +3,25 @@ import Image from 'next/image'
 export type PlatformItems = {
   alt: string
   caption: string
+  platformAccountName: string
   src: string
 }
 interface Props extends PlatformItems {
   handleSelectItem: (item: PlatformItems) => void
 }
 
-const PlatformItem: React.FC<Props> = ({ alt, caption, handleSelectItem, src }) => {
+const PlatformItem: React.FC<Props> = ({
+  alt,
+  caption,
+  handleSelectItem,
+  platformAccountName,
+  src,
+}) => {
   return (
-    <button className='w-20' onClick={() => handleSelectItem({ alt, caption, src })}>
+    <button
+      className='w-20'
+      onClick={() => handleSelectItem({ alt, caption, platformAccountName, src })}
+    >
       <div className='flex h-20 w-20 items-center justify-center rounded-xl bg-stone-100'>
         <Image alt={alt} className='rounded' height='48' src={src} width='48' />
       </div>
